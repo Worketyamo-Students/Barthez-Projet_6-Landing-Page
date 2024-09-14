757d51af6779b10f3e57db50519c51ac9dc6d9b2
01Barthez <kenwoubarthez@gmail.com>
2024-09-14 07:55:14 +0100

    start from the begin

--- script.js
+++
@@ -1,7 +0,0 @@
-const chevronsDown = document.querySelectorAll("i.fa-chevron-down");
-
-chevronsDown.forEach((chevron)=>{
-    chevron.addEventListener("click", ()=>{
-        chevron.nextElementSibling.style.display = chevron.nextElementSibling.style.display === 'flex' ? 'none':'flex';
-    })
-})
